569a3e06714e81c598b4f9340fd877a158dac0ec
Sachin4219 <elzachin373@gmail.com>
2023-09-19 13:00:44 +0530

    added chat feature

--- client/src/pages/HomePageUser.jsx
+++ client/src/pages/HomePageUser.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ChatLobby from '../components/ChatLobby.jsx'
+import ChatRoom from '../components/ChatRoom.jsx';
 
 const HomePageUser = () => {
+  const [isActive, setIsActive] = React.useState("false");
+  const activateChatRoom = JSON.parse(localStorage.getItem("activeChat"));
   return (
-    <div>HomePageUser</div>
+    <div>
+      {activateChatRoom ? <ChatRoom/> : <ChatLobby/>}
+    </div>
   )
 }
 
