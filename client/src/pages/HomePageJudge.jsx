62460a4ea2c1056b00add8697c090097950d3a9d
Aarti <aartiarora1230@gmail.com>
2023-09-19 16:30:30 +0530

    user's page

--- client/src/pages/HomePageJudge.jsx
+++ client/src/pages/HomePageJudge.jsx
@@ -249,7 +249,7 @@ const HomePageJudge = () => {
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
 
         {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} profile="Judge" subprofile="Adminstration"/>
 
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
--- client/src/partials/Header.jsx
+++ client/src/partials/Header.jsx
@@ -7,7 +7,9 @@ import UserMenu from '../components/DropdownProfile';
 
 function Header({
   sidebarOpen,
-  setSidebarOpen
+  setSidebarOpen,
+  profile,
+  subprofile
 }) {
 
   const [searchModalOpen, setSearchModalOpen] = useState(false)
@@ -57,7 +59,7 @@ function Header({
             <Help align="right" /> */}
             {/*  Divider */}
             <hr className="w-px h-6 bg-slate-200 mx-3" />
-            <UserMenu align="right" />
+            <UserMenu profile={profile} subprofile={subprofile} align="right" />
 
           </div>
 
