b83c7a62bffa74334a92e29f6c1f1e74387d43e9
VaishaliAgg14 <vaishali.aggarwal1410@gmail.com>
2023-09-19 23:58:19 +0530

    calender route

--- client/src/partials/Sidebar.jsx
+++ client/src/partials/Sidebar.jsx
@@ -342,7 +342,7 @@ function Sidebar({
               <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${pathname.includes('calendar') && 'bg-slate-900'}`}>
                 <NavLink
                   end
-                  to="/calendar"
+                  to="/judge/calendar"
                   className={`block text-slate-200 truncate transition duration-150 ${
                     pathname.includes('calendar') ? 'hover:text-slate-200' : 'hover:text-white'
                   }`}
